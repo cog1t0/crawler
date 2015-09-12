f2a9b21a5e8603c7507b3d3225acb3843b6186ed
Yamazumi <yhide5883@icloud.com>
2015-09-12 17:25:57 +0900

    git pull前のコミット

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,4 +6,14 @@
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
 
-# XXXXXX.create(:aaa => 'ああ',:bbb =>'いい')
+Category.create(category_map_id: 1, title: '癒し')
+Category.create(category_map_id: 2, title: '遊び')
+Category.create(category_map_id: 3, title: '食べる')
+
+Map.create(category_map_id: 1, title: '松江歴史館', description: '説明文説明文', url: 'http://xxxxxxxxxx')
+Map.create(category_map_id: 2, title: '松江ホーランエンヤ伝承館', description: '説明文説明文', url: 'http://yyyyyyyyyy')
+Map.create(category_map_id: 3, title: 'Greens Baby', description: '説明文説明文', url: 'http://ggggggggggg')
+
+Category_map.create(category_id 1, map_id: 1)
+Category_map.create(category_id 2, map_id: 2)
+Category_map.create(category_id 3, map_id: 3)
